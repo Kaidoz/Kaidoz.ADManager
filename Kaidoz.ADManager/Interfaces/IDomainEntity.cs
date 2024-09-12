e76029bebff76f9b89a6e9ae9ac60e3f7c486a63
Timur <33166919+Kaidoz@users.noreply.github.com>
2024-09-12 13:44:10 +0300

    Interface for abstract class

---
+++ Kaidoz.ADManager/Interfaces/IDomainEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaidoz.ADManager.Interfaces
+{
+    public interface IDomainEntity 
+    {
+        public void ParseProperties(Principal principal);
+
+        public void SaveProperties(Principal principal);
+    }
+}
