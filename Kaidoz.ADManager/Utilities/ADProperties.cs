4b071370d9424d43989ca93e4a82cc3ede750308
Timur <33166919+Kaidoz@users.noreply.github.com>
2024-09-12 13:44:53 +0300

    Added enum state flags for entities and added class with names of entity ad properties.

---
+++ Kaidoz.ADManager/Utilities/ADProperties.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace Kaidoz.ADManager.Utilities
+{
+    public sealed class ADProperties
+    {
+        public const string ObjectGuid = "objectGUID";
+        public const string ObjectSid = "objectSID";
+        public const string CommonName = "cn";
+        public const string DistinguishedName = "distinguishedName";
+        public const string UserPrincipalName = "userPrincipalName";
+        public const string SamAccountName = "sAMAccountName";
+        public const string GivenName = "givenName";
+        public const string Surname = "sn";
+        public const string DisplayName = "displayName";
+        public const string Email = "mail";
+        public const string TelephoneNumber = "telephoneNumber";
+        public const string MemberOf = "memberOf";
+        public const string Description = "description";
+        public const string AccountExpires = "accountExpires";
+        public const string LastLogon = "lastLogon";
+        public const string UserAccountControl = "userAccountControl";
+        public const string HomeDirectory = "homeDirectory";
+        public const string HomeDrive = "homeDrive";
+        public const string Manager = "manager";
+        public const string Created = "whenCreated";
+        public const string Modified = "whenChanged";
+        public const string LastName = "sn";
+        public const string FirstName = "givenName";
+        public const string PrincipalName = "userPrincipalName";
+        public const string TelephoneHome = "homePhone";
+        public const string TelephoneMobile = "mobile";
+        public const string Country = "country";
+        public const string City = "l";
+        public const string State = "st";
+        public const string PostalCode = "postalCode";
+        public const string StreetAddress = "streetAddress";
+        public const string Company = "company";
+        public const string Title = "title";
+        public const string InternetAccess = "proxyAddresses";
+        public const string ManagerDistinguishedName = "manager";
+        public const string Department = "department";
+        public const string Mobile = "mobile";
+        public const string IpTelephone = "ipPhone";
+        public const string HomePhone = "homePhone";
+    }
+}
---
+++ Kaidoz.ADManager/Utilities/Enums/EntityControlFlags.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaidoz.ADManager.Utilities.Enums
+{
+    [Flags]
+    public enum EntityControlFlags
+    {
+        SCRIPT = 0x0001,
+        ACCOUNTDISABLE = 0x0002,
+        HOMEDIR_REQUIRED = 0x0008,
+        LOCKOUT = 0x0010,
+        PASSWD_NOTREQD = 0x0020,
+        PASSWD_CANT_CHANGE = 0x0040,
+        ENCRYPTED_TEXT_PASSWORD_ALLOWED = 0x0080,
+        TEMP_DUPLICATE_ACCOUNT = 0x0100,
+        NORMAL_ACCOUNT = 0x0200,
+        INTERDOMAIN_TRUST_ACCOUNT = 0x0800,
+        WORKSTATION_TRUST_ACCOUNT = 0x1000,
+        SERVER_TRUST_ACCOUNT = 0x2000,
+        DONT_EXPIRE_PASSWORD = 0x10000,
+        PASSWORD_EXPIRED = 0x80000,
+        TRUSTED_FOR_DELEGATION = 0x200000,
+        NOT_DELEGATED = 0x400000,
+        USE_DES_KEY_ONLY = 0x800000,
+        DONT_REQUIRE_PREAUTH = 0x1000000,
+        TRUSTED_TO_AUTHENTICATE_FOR_DELEGATION = 0x2000000
+    }
+}
