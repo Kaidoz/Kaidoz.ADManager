b1856f087e1dfd3d008ad401ded45b9f56a634b3
Timur <33166919+Kaidoz@users.noreply.github.com>
2024-11-05 17:58:53 +0300

    Moved context to namespace 'Contextx'

---
+++ Kaidoz.ADManager/Contexts/UserContext.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using KDZ.ActiveDirectory.Models;
+
+namespace KDZ.ActiveDirectory.Manager
+{
+    public class UserContext : IDisposable
+    {
+        private readonly PrincipalContext Context;
+
+        private readonly UserPrincipal Principal;
+
+        public UserContext(string domain, string login, string password)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(domain, nameof(domain));
+            ArgumentException.ThrowIfNullOrEmpty(login, nameof(login));
+            ArgumentException.ThrowIfNullOrEmpty(password, nameof(password));
+
+            Context = new PrincipalContext(ContextType.Domain, domain, login, password);
+
+            Principal = new UserPrincipal(Context);
+        }
+
+        public void Dispose()
+        {
+            if (Principal != null)
+                Principal.Dispose();
+
+            if (Context != null)
+                Context.Dispose();
+        }
+
+        public UserPrincipal? GetUserOrDefault(string accountName)
+        {
+            try
+            {
+
+                var user = UserPrincipal.FindByIdentity(Context, accountName);
+
+                return user;
+            }
+            catch
+            {
+                return default;
+            }
+        }
+
+        public UserPrincipal GetUser(string accountName)
+        {
+            var user = UserPrincipal.FindByIdentity(Context, accountName);
+
+            return user;
+        }
+
+        public List<DomainUser> GetUsers(string groupName)
+        {
+            var list = new List<DomainUser>();
+
+            GroupPrincipal group = GroupPrincipal.FindByIdentity(Context, groupName);
+
+            if (group != null)
+            {
+                list.AddRange(group.GetMembers()
+                    .Where(userPrincipal => userPrincipal != null)
+                    .Select(userPrincipal => new DomainUser((UserPrincipal)userPrincipal)));
+            }
+
+            return list;
+        }
+
+        public void SetDescription(string accountName, string text)
+        {
+            try
+            {
+                Principal.SamAccountName = accountName;
+
+                using PrincipalSearcher search = new PrincipalSearcher(Principal);
+
+                if (search == null)
+                    return;
+
+                var result = search.FindOne();
+
+                if (result == null)
+                {
+                    return;
+                }
+
+                UserPrincipal userResult = (UserPrincipal)result;
+
+                var obj = userResult.GetUnderlyingObject();
+
+                if (obj == null)
+                    return;
+
+                DirectoryEntry directoryEntry = (DirectoryEntry)obj;
+
+                directoryEntry.Properties["description"].Value = text;
+
+                directoryEntry.CommitChanges();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
